9bea7155ee26458fb947c6c21d008e4c8f5b718a
Denis Behrends <denis-bd@outlook.de>
2019-01-16 22:35:16 +0100

    Dispatch muster exakt

--- Meilenstein4/Paket6/emensa/Controllers/DispatchController.cs
+++ Meilenstein4/Paket6/emensa/Controllers/DispatchController.cs
@@ -29,25 +29,16 @@ namespace emensa.Controllers
                 
                 var _c = _context; 
 
-                var bestellungenListe = from _benutzer in _c.Benutzer
-                    join _bestellung in _c.Bestellungen on  _benutzer.Nummer equals _bestellung.BenutzerNummer
-                    join _bestellungenthaltmahlzeit in _c.BestellungEnthältMahlzeit on _bestellung.Nummer equals _bestellungenthaltmahlzeit.FkBestellungen
-                    join _mahlzeit in _c.Mahlzeiten on _bestellungenthaltmahlzeit.FkMahlzeit equals _mahlzeit.Id
-                    join _kategorie in _c.Kategorien on _mahlzeit.FkKategorie equals _kategorie.Id
+                var bestellungenListe = 
+                    from _bestellung in _c.Bestellungen
                     group _bestellung by _bestellung.Nummer into bestellung
                     let b = new { frist = bestellung.First() }
-                    let m = new { mahlzeiten=b.frist.BestellungEnthältMahlzeit }
+                    let bem = _c.BestellungEnthältMahlzeit.Where(x=> x.FkBestellungen == b.frist.Nummer)
+                    let Mahlzeiten = bem.Join(_c.Mahlzeiten, (bems => bems.FkMahlzeit), (mz => mz.Id),(bems, mz) 
+                                                    => new { Mahlzeit = new { Anzahl=bems.Anzahl, Name=mz.Name, Vorrat= mz.Vorrat, Kategorie=mz.FkKategorieNavigation.Bezeichnung} }) 
                     let benutzer = new {b.frist.BenutzerNummerNavigation.Vorname,b.frist.BenutzerNummerNavigation.Nachname,b.frist.BenutzerNummerNavigation.Nutzername,b.frist.BenutzerNummerNavigation.EMail}
-                    select new{ benutzer, b.frist.Abholzeitpunkt, b.frist.BestellZeitpunkt, m.mahlzeiten };
-                    /*let benutzer = new { Vorname=_benutzer.Vorname,Nachname=_benutzer.Nachname,Nutzername=_benutzer.Nutzername,EMail=_benutzer.EMail }
-                    let mahlzeiten = new { _mahlzeit.Name, _mahlzeit.MahlzeitenZutaten, _mahlzeit.FkKategorieNavigation}
-                    select new {benutzer,Abholung=_bestellung.Abholzeitpunkt,Bestellnummer=_bestellung.Nummer, mahlzeiten};
-                    "Vorname": "Bugs",
-                    "Nachname": "Findmore",
-                    "Nutzername": "bugfin",
-                    "EMail": "dbwt2018@ismypassword.com"
-                    
-                    */
+                    select new{ Benutzer=benutzer, Abholung=b.frist.Abholzeitpunkt, Bestellnummer=b.frist.Nummer, Mahlzeiten };
+
                 return Json(bestellungenListe);
                 
             }
